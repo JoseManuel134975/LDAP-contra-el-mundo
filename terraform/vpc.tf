c7cc0510294afb8fdecbe9f87a028bde9b0d9a73
JoseManuel134975 <josemanuel3606@gmail.com>
2025-05-13 16:17:02 +0200

    Error menor en vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "allow_instance_ports" {
 
   ingress {
     from_port   = 8443
-    to_port     = 8433
+    to_port     = 8443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
